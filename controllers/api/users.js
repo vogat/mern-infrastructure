be6018568f40bb701467e311ba62cdb41de0ebe0
Jim Clark <jim.clark@generalassemb.ly>
2022-12-01 11:30:19 -0800

    sync-16-part-5-finish

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,28 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+
 module.exports = {
   create
 };
 
-function create(req, res) {
-  // Baby step...
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email
-    }
-  });
+async function create(req, res) {
+  try {
+    // Add the user to the db
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    res.json(token);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+/*--- Helper Functions --*/
+
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  );
 }
