d4184a7106d0120526bac93109525830ad22440f
Jim Clark <jim.clark@generalassemb.ly>
2022-12-05 13:47:30 -0800

    sync-18-part-6-update-state-at-load

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
@@ -7,13 +8,13 @@ import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
       { user ?
           <>
-            <NavBar />
+            <NavBar user={user} />
             <Routes>
               {/* Route components in here */}
               <Route path="/orders/new" element={<NewOrderPage />} />
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -7,6 +7,26 @@ import * as usersAPI from './users-api';
 
 export async function signUp(userData) {
   const token = await usersAPI.signUp(userData);
+  localStorage.setItem('token', token);
   // TODO: return user object from token instead
   return token;
+}
+
+export function getToken() {
+  // getItem will return null if the key does not exists
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  // A JWT's exp is expressed in seconds, not miliseconds
+  if (payload.exp * 1000 < Date.now()) {
+    // Token has expired
+    localStorage.removeItem('token');
+    return null;
+  }
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
