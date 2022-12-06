752e12310e404c7f242ee2efc8add141fb3a38fa
Jim Clark <jim.clark@generalassemb.ly>
2022-12-06 10:57:47 -0800

    sync-23-part-7-check-token

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,5 +8,7 @@ const usersCtrl = require('../../controllers/api/users');
 router.post('/', usersCtrl.create);
 // POST /api/users/login
 router.post('/login', usersCtrl.login);
+// GET /api/users/check-token
+router.get('/check-token', usersCtrl.checkToken);
 
 module.exports = router;
--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -3,6 +3,7 @@ import { checkToken } from '../../utilities/users-service';
 export default function OrderHistoryPage() {
   async function handleCheckToken() {
     const expDate = await checkToken();
+    console.log(expDate);
   }
   
   return (
